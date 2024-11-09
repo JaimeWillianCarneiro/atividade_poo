5e84d5b259d2ee39a13c01c15990218dd16349bd
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-11-09 11:39:27 -0300

    adicionando relações

--- classes.py
+++ classes.py
@@ -3,18 +3,23 @@ Arquivo com as classes a serem construidas
 """
 from enum import Enum
 
+"""  
+* Há uma relação de AGREGAÇÃO entre Funcionario e Loja e Funcinario , 
+poois pode existir um funcionario independente da sua loja 
+
+"""
 class Loja:
     def __init__(self, localizacao):
         
         self.localizacao = localizacao
         self.funcionarios = [] # A loja agrega os funcionarios
-        self.estoque = [] # A loga agrega o estoque
+        self.estoque = [] 
     
     def adicionar_funcionario(self, funcionario):
         self.funcionarios.append(funcionario)
     
-    def adicionar_instrumento(self, instrumento):
-        self.estoque.append(instrumento)
+    def adicionar_instrumento(self,  marca, modelo, preco, numero_cordas):
+        self.estoque.append(Instrumento(marca, modelo, preco, numero_cordas))
     
     def consultar_funcionarios(self):
         return self.funcionarios
@@ -27,6 +32,24 @@ class Funcionario:
         self.loja_atual = None
         self.cargo = cargo # Ver a hierarquia depois 
 
+
+def TipoInstrumento(Enum):
+    GUITARRA = "guitarra"
+    BAIXO = "baixo"
+    VIOLAO = "violão"
+
 class Instrumento:
+    opcoes_instrumentos = list(TipoInstrumento)
+    
+    def __init__(self, marca, modelo, preco, numero_cordas ):
+        self.marca
+        self.preco
+        self.numero_cordas
+        self.modelo
+    
+class Guitarra(Instrumento):
     pass
-        
+
+
+info_violao = ['marca': ]
+violao_1 = Instrumento()
