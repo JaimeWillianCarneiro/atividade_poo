bf30b62b74c525d4b889d0144bdc73f0ce2fb6a3
JaimeWillianCarneiro <jwcs2@aluno.ifal.edu.br>
2024-11-10 20:30:12 -0300

    adicionando metodo de adicionar loja mais perto na classes loja

--- classes.py
+++ classes.py
@@ -15,6 +15,7 @@ class Loja:
         self.localizacao = localizacao
         self.funcionarios = [] # A loja agrega os funcionarios
         self.estoque = [] 
+        self.loja_mais_perto = None
     
     def __str__(self):
         return f"Localização da Loja: {self.localizacao} \nNúmero de funcionários: {len(self.funcionarios)}\n"
@@ -23,12 +24,19 @@ class Loja:
     def adicionar_funcionario(self, funcionario):
         self.funcionarios.append(funcionario)
     
+    def remover_funcionario(self):
+        #  removendo o último funcionário adicionado
+        self.funcionarios.remove(self.consultar_funcionarios[-1])
+    
     def adicionar_instrumento(self,  marca, modelo, preco, numero_cordas):
         self.estoque.append(Instrumento(marca, modelo, preco, numero_cordas))
     
     def consultar_funcionarios(self):
         return self.funcionarios
-
+    
+    def set_loja_mais_proxima(self, loja_mais_proxima):
+        self.loja_mais_perto = loja_mais_proxima.localizacao
+        
 class Funcionario:
     def __init__(self, nome_completo, cpf, salario, cargo):
         self.nome_completo = nome_completo
@@ -44,6 +52,7 @@ class Funcionario:
             return f"Nome completo: {self.nome_completo}, Cargo: {self.cargo}"
     def set_loja(self, loja):
         self.loja_atual= loja
+    
         
 
 def TipoInstrumento(Enum):
